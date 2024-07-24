bf8deac893317b18227e9d783c8f3c73c3a09c19
Rana Hamdy <104355652+ranahamdy1@users.noreply.github.com>
2024-07-24 19:16:09 +0300

    Create inheritace.dart
    inheritace

---
+++ oop/inheritace.dart
@@ -0,0 +1,16 @@
+void main(){
+  Person p = Person();
+  p.speak();
+  p.walk();
+}
+
+class Human{
+  void walk(){
+    print("walk");
+  }
+}
+class Person extends Human{
+  void speak(){
+    print("speak");
+  }
+}
