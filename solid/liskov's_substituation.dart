b419fa297a957a0bdabc81f9c66f253624c62a3b
Rana Hamdy <104355652+ranahamdy1@users.noreply.github.com>
2024-07-25 17:07:42 +0300

    Update liskov's_substituation.dart
    edit

--- solid/liskov's_substituation.dart
+++ solid/liskov's_substituation.dart
@@ -1,20 +1,22 @@
+import 'dart:math';
+
 abstract class Shape {
   double area();
 }
 
 class Rectangle implements Shape {
-  double width;
-  double height;
+  double? width;
+  double? height;
 
   double area() {
-    return width * height;
+    return width! * height!;
   }
 }
 
 class Square implements Shape {
-  double side;
+  double? side;
 
   double area() {
-    return pow(side, 2);
+    return pow(side!, 2) as double;
   }
 }
