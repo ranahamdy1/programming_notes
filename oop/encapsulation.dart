255eb03f0f592ffefa9ef1ffa015273540cdbd98
Rana Hamdy <104355652+ranahamdy1@users.noreply.github.com>
2024-08-21 19:29:08 +0300

    Update encapsulation.dart
    Update encapsulation.dart

--- oop/encapsulation.dart
+++ oop/encapsulation.dart
@@ -1,13 +1,23 @@
-void main() {
-  Customer customer = Customer();
-  customer.setName = 'jo';
-  print(customer.getName);
+class Animal {
+  String _name;
+
+  Animal(this._name);
+
+  // Setter method
+  set name(String name) {
+    _name = name;
+  }
+
+  // Getter method
+  String get name {
+    return _name;
+  }
 }
 
-class Customer {
-  String? _name;
+void main() {
+  Animal object = Animal('omar');
 
-  set setName(String name) => _name = name;
-
-  String get getName => _name!;
+  // Accessing the private variable through the getter
+  String lionName = object.name;
+  print(lionName);
 }
