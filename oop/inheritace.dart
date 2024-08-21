a3332296b531960bf71e8b3aeeb997a8a27a7635
Rana Hamdy <104355652+ranahamdy1@users.noreply.github.com>
2024-08-21 19:45:53 +0300

    Update inheritace.dart
    Update inheritace.dart

--- oop/inheritace.dart
+++ oop/inheritace.dart
@@ -1,21 +1,46 @@
-void main(){
-  Person p = Person();
-  p.speak();
-  p.walk();
+class Animal {
+  String _name;
+
+  Animal(this._name);
+
+  // Method to simulate eating behavior for the Animal class
+  void eat() {
+    print("lion eat");
+  }
+
+  // Setter method
+  set name(String name) {
+    _name = name;
+  }
+
+  // Getter method
+  String get name {
+    return _name;
+  }
 }
 
-class Human{
-  void walk(){
-    print("walk");
+class Birds extends Animal {
+  // Constructor that calls the Animal constructor with the name argument
+  Birds(super.name);
+
+  // Overriding the eat method in the Animal class
+  @override
+  void eat() {
+    print("birds eat");
   }
 }
-class Person extends Human{
-  void speak(){
-    print("speak");
-  }
-  //override function
-  @override
-  void walk(){
-    print("change words here override walk");
-  }
+
+void main() {
+  Animal object = Animal('omar');
+  Birds birds = Birds("birds name");
+
+  // Accessing the private variable through the getter
+  String lionName = object.name;
+  String birdsName = birds.name;
+
+  print(lionName);
+  object.eat();
+
+  print(birdsName);
+  birds.eat();
 }
