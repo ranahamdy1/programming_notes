8d379a57dc3ef8611cf5abd8f7fb1ca3206f11a6
Rana Hamdy <104355652+ranahamdy1@users.noreply.github.com>
2024-08-21 20:00:49 +0300

    Update abstraction.dart
    Update abstraction.dart

--- oop/abstraction.dart
+++ oop/abstraction.dart
@@ -1,20 +1,46 @@
-void main() {
-  var human = Student();
-  print(human.name);
+abstract class Animal {
+  String _name;
+  Animal(this._name);
+  // Abstract method to simulate eating behavior
+  void eat();
+  // Abstract method to simulate drinking behavior
+  void drink();
+
+  // Setter method for _name
+  set name(String name) {
+    _name = name;
+  }
+  // Getter method for _name
+  String get name {
+    return _name;
+  }
 }
 
-abstract class Human {
-  String? name = 'rana';
-  //abstract method
-   void drink();
-   void eat();
-}
-class Student extends Human {
+class Birds extends Animal {
+  // Constructor that calls the Animal constructor with the name argument
+  Birds(super.name);
+  // Implementing the abstract eat method in the Animal class
+  @override
+  void eat() {
+    print("birds eat");
+  }
+  // Implementing the abstract drink method in the Animal class
   @override
   void drink() {
-    print('student drink');
+    print('bird drink');
   }
+}
 
-  @override
-  void eat() {}
+void main() {
+  Birds birds = Birds("birds name");
+  String birdsName = birds.name;
+  print(birdsName);
+  birds.eat();
+
+  // Cannot instantiate an abstract class
+  // Animal object = Animal('omar');
+  // Accessing the private variable through the getter
+  // String lionName = object.name;
+  // print(lionName);
+  // object.eat();
 }
