f5f11cec61009b6e68186b8f032c044196ebb104
Rana Hamdy <104355652+ranahamdy1@users.noreply.github.com>
2024-08-24 01:05:02 +0300

    Update inheritace.dart
    Update inheritace.dart

--- oop/inheritace.dart
+++ oop/inheritace.dart
@@ -1,33 +1,30 @@
 class Animal {
-  String? _name;
-  
-  void eat() {
-    print("lion eat");
-  }
-  // Setter method
-  set setName(String name) {
-    _name = name;
-  }
-  // Getter method
-  String? get getName {
-    return _name;
-  }
+String _name;
+Animal(this._name);
+
+// Setter method
+set setName(String name) {
+  _name = name;
 }
 
-class Birds extends Animal {
-  //take all from Animal class //inheritance
+// Getter method
+String get getName {
+  return _name;
+}
+}
+
+class Birds extends Animal{
+  Birds(super.name);
+//take all from Animal class //inheritance
 }
 
 void main() {
-  Animal lionName = Animal();
-  // printing through the getter
-  lionName.setName = "lion";
+  Animal lionName = Animal('omar');
+  print(lionName._name);
+  //to update name by setter and getter
+  lionName.setName = "ali";
   print(lionName.getName);
-  lionName.eat();
 
-  Birds birdsName = Birds();
-  // printing through the getter
-  birdsName.setName = "birds";
-  print(birdsName.getName);
-  birdsName.eat();
+  Birds birdsName = Birds("birds");
+  print(birdsName._name);
 }
