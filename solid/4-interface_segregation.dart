5e3749d76013cb89a663d9306e7c86848e441e6a
Rana Hamdy <104355652+ranahamdy1@users.noreply.github.com>
2024-08-27 22:55:19 +0300

    Update 4-interface_segregation.dart

--- solid/4-interface_segregation.dart
+++ solid/4-interface_segregation.dart
@@ -10,3 +10,55 @@ abstract class FlyingAnimal {
 abstract class WalkingAnimal {
   void walk();
 }
+
+
+
+//this single responsibility principle of the same code
+/*abstract class BasicAnimalBehavior {
+  void eat();
+  void drink();
+}
+
+abstract class Flyable {
+  void fly();
+}
+
+abstract class Walkable {
+  void walk();
+}
+
+// A bird can eat, drink, and fly
+class Bird implements BasicAnimalBehavior, Flyable {
+  @override
+  void eat() {
+    print("Bird eats");
+  }
+
+  @override
+  void drink() {
+    print("Bird drinks");
+  }
+
+  @override
+  void fly() {
+    print("Bird flies");
+  }
+}
+
+// A dog can eat, drink, and walk
+class Dog implements BasicAnimalBehavior, Walkable {
+  @override
+  void eat() {
+    print("Dog eats");
+  }
+
+  @override
+  void drink() {
+    print("Dog drinks");
+  }
+
+  @override
+  void walk() {
+    print("Dog walks");
+  }
+}*/
