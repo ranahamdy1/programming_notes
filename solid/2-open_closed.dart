070ac6398afe8ca6cb0c057fee5982f49edb10a6
Rana Hamdy <104355652+ranahamdy1@users.noreply.github.com>
2024-09-07 14:44:19 +0300

    Update 2-open_closed.dart

--- solid/2-open_closed.dart
+++ solid/2-open_closed.dart
@@ -1,11 +1,16 @@
 import 'dart:math';
 
+// Abstract class 'Shape' defines a common interface for all shapes.
+// This design is open for extension but closed for modification.
 abstract class Shape {
+  // Method 'area' must be implemented by all shapes.
   double area();
 }
 
+// 'Rectangle' implements 'Shape', providing a specific implementation for rectangles.
+// Adding new shapes (e.g., Triangle, Polygon, Circle) does not require modification to this class.
 class Rectangle implements Shape {
-  double? width;
+  double? width; 
   double? height;
 
   @override
@@ -14,11 +19,13 @@ class Rectangle implements Shape {
   }
 }
 
+// 'Circle' implements 'Shape', providing a specific implementation for circles.
+// Adding new shapes (e.g., Triangle, Polygon) does not require modification to this class.
 class Circle implements Shape {
-  double? radius;
+  double? radius; 
 
   @override
   double area() {
-    return pi * pow(radius!, 2);
+    return pi * pow(radius!, 2); 
   }
 }
