cc9e8fe06193e913c111d64d1ab7de2b0ffc6903
Rana Hamdy <104355652+ranahamdy1@users.noreply.github.com>
2024-09-07 14:36:00 +0300

    Update 1-single_responsability.dart

--- solid/1-single_responsability.dart
+++ solid/1-single_responsability.dart
@@ -1,13 +1,18 @@
+//The User class is responsible only for holding user data.
 class User {
   int? id;
   String? name;
+
+  User({this.id, this.name});
 }
 
+// The UserRepository class is responsible only for saving User objects
 class UserRepository {
   void save(User user) {
   }
 }
 
+//The EmailService class is responsible only for sending emails to User objects
 class EmailService {
   void sendEmail(User user) {
   }
