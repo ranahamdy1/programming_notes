bcfc7453bcc14524aa895720c764b36d9119f9ab
Rana Hamdy <104355652+ranahamdy1@users.noreply.github.com>
2024-09-09 00:34:24 +0300

    Update 2-facade_pattern.dart

--- design_patterns/2-structural/2-facade_pattern.dart
+++ design_patterns/2-structural/2-facade_pattern.dart
@@ -1,1 +1,43 @@
-//inprogress
+// الفئات المعقدة
+class CustomerData {
+  void getCustomerDetails() {
+    print("Fetching customer details...");
+  }
+}
+
+class OrderProcessor {
+  void processOrder() {
+    print("Processing order...");
+  }
+}
+
+class EmailSender {
+  void sendConfirmationEmail() {
+    print("Sending confirmation email...");
+  }
+}
+
+// فئة الواجهة (Facade)
+class OrderFacade {
+  final CustomerData _customerData;
+  final OrderProcessor _orderProcessor;
+  final EmailSender _emailSender;
+
+  OrderFacade(this._customerData, this._orderProcessor, this._emailSender);
+
+  void placeOrder() {
+    _customerData.getCustomerDetails();
+    _orderProcessor.processOrder();
+    _emailSender.sendConfirmationEmail();
+    print("Order placed successfully!");
+  }
+}
+
+void main() {
+  final customerData = CustomerData();
+  final orderProcessor = OrderProcessor();
+  final emailSender = EmailSender();
+
+  final orderFacade = OrderFacade(customerData, orderProcessor, emailSender);
+  orderFacade.placeOrder();
+}
