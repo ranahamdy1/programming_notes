ea4ac267e46aaf226616d3648e5cab88850d8c7e
Rana Hamdy <104355652+ranahamdy1@users.noreply.github.com>
2024-09-08 20:28:27 +0300

    Update 1-adapter_pattern.dart

--- design_patterns/2-structural/1-adapter_pattern.dart
+++ design_patterns/2-structural/1-adapter_pattern.dart
@@ -1,22 +1,36 @@
+//  هما فئتان توفران واجهات مختلفة LightningPort و UsbPort
+
+//Lightning فئة تدعم منفذ 
 class LightningPort {
   void connectLightning() {
-    print("Connected with Lightning Port");
+    print("تم الاتصال عبر منفذ Lightning");
   }
 }
 
+//USB فئة تدعم منفذ 
 class UsbPort {
   void connectUsb() {
-    print("Connected with USB Port");
+    print("تم الاتصال عبر منفذ USB");
   }
 }
 
 // Adapter class
+// Lightning متوافقًا مع منفذ USB فئة المحول التي تجعل منفذ
+// UsbPort ولكنها في الواقع تستخدم كائنًا من فئة LightningPort يعمل كمحول، حيث يقوم بتنفيذ واجهة 
 class LightningToUsbAdapter implements LightningPort {
   UsbPort usbPort;
   LightningToUsbAdapter(this.usbPort);
 
   @override
   void connectLightning() {
-    usbPort.connectUsb();  // Adapting the UsbPort method to LightningPort
+    usbPort.connectUsb();  // تكييف طريقة الاتصال عبر USB لتعمل كأنها اتصال عبر Lightning
   }
 }
+
+void main() {
+  UsbPort usbPort = UsbPort();
+  LightningPort lightningPort = LightningToUsbAdapter(usbPort);
+
+  lightningPort.connectLightning(); // "تم الاتصال عبر منفذ USB" :ستطبع 
+}
+
