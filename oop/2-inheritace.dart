770f669b022dbcc5abc18b87b6b5ed631115d342
Rana Hamdy <104355652+ranahamdy1@users.noreply.github.com>
2025-04-17 13:18:24 +0200

    Update 2-inheritace.dart

--- oop/2-inheritace.dart
+++ oop/2-inheritace.dart
@@ -1,30 +1,28 @@
-class Animal {
-  String _name;
-  Animal(this._name);
+class Human {
+  String? _email;
 
 // Setter method
-  set setName(String name) {
-    _name = name;
-  }
+  set setEmail(String email) {
+    if(email.contains('@')){
+      _email = email;
+    }else{
+      print('Invalid email: must contain "@"');
+    }
+    }
 
 // Getter method
-  String get getName {
-    return _name;
+  String? get getEmail {
+    return _email;
   }
-}
-
-class Birds extends Animal{
-  Birds(super.name);
-//take all from Animal class //inheritance
 }
 
 void main() {
-  Animal lionName = Animal('omar');
-  print(lionName.getName);
-  //to update name by setter and getter
-  lionName.setName = "ali";
-  print(lionName.getName);//getName not _name
+  Human object = Human();
+  print(object.getEmail);            //null
 
-  Birds birdsName = Birds("birds");
-  print(birdsName.getName);//getName not _name
+  //to update name by setter and getter
+  object.setEmail = "ali";           //null
+  print(object.getEmail);            //Invalid email: must contain "@"
+  object.setEmail = "ali@gmail.com"; //valid
+  print(object.getEmail);            //ali@gmail.com
 }
